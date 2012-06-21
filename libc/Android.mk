6440aa180d1cd0c17ae8bc0b6537cb9a782eab45
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-06-21 16:44:14 -0500

    libc: update inclusion logic for linaro string routines
    Make it opt-in with: TARGET_USE_LINARO_STRING_ROUTINES:=tru
    Don't override the scorpion/krait mem(cpy/set) optimizations
    
    Change-Id: I69f58b8a98bcc79fe596039d252d1db70effcb05

--- libc/Android.mk
+++ libc/Android.mk
@@ -404,23 +404,42 @@ libc_arch_static_src_files := \
 libc_arch_dynamic_src_files := \
 	arch-arm/bionic/exidx_dynamic.c
 
-ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+# linaro optimized string routines are opt-in
+TARGET_USE_LINARO_STRING_ROUTINES ?= false
+
+# We can only use linaro optimizations on Arm-v7a
+ifeq ($(TARGET_USE_LINARO_STRING_ROUTINES)-$(ARCH_ARM_HAVE_ARMV7A),true-true)
 libc_common_src_files += \
 	arch-arm/bionic/armv7/memchr.S \
-	arch-arm/bionic/armv7/memcpy.S \
-	arch-arm/bionic/armv7/memset.S \
-	arch-arm/bionic/armv7/bzero.S \
 	arch-arm/bionic/armv7/strchr.S \
 	arch-arm/bionic/armv7/strcpy.c \
 	arch-arm/bionic/armv7/strlen.S
 else
 libc_common_src_files += \
 	string/memchr.c \
-	arch-arm/bionic/memcpy.S \
-	arch-arm/bionic/memset.S \
 	string/strchr.c \
 	arch-arm/bionic/strcpy.S \
 	arch-arm/bionic/strlen.c.arm
+endif
+
+# Don't override Qcom optimizations with linaro
+ifeq ($(TARGET_USE_SCORPION_BIONIC_OPTIMIZATION),true)
+libc_common_src_files += \
+	arch-arm/bionic/memcpy.S \
+	arch-arm/bionic/memset.S
+else ifeq ($(TARGET_USE_KRAIT_BIONIC_OPTIMIZATION),true)
+libc_common_src_files += \
+	arch-arm/bionic/memcpy.S \
+	arch-arm/bionic/memset.S
+else ifeq ($(TARGET_USE_LINARO_STRING_ROUTINES)-$(ARCH_ARM_HAVE_ARMV7A),true-true)
+libc_common_src_files += \
+	arch-arm/bionic/armv7/memcpy.S \
+	arch-arm/bionic/armv7/memset.S \
+	arch-arm/bionic/armv7/bzero.S
+else # No mem optimizations
+libc_common_src_files += \
+	arch-arm/bionic/memcpy.S \
+	arch-arm/bionic/memset.S
 endif
 
 else # !arm
