db7de859e093907b1f06aae92338410507d5813f
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-23 12:27:25 +0700

    linker: restore prelink support
    Prelink support is required to load old vendor binary blobs
    on many devices properly
    
    This commit partially reverts 4688279db5dcc4004941e7f133c4a1c3617d842c
    
     This is the 2nd commit message:
    
    bionic: linker: fix prelink support
    
    load_bias_ needs to be calculated from the min_vaddr, not the
    prelink address
    
    Fixes
    
    couldn't map "libraryname.so" segment 1: Invalid argument
    
    Change-Id: Ibf1fd1d5a0f2c81ce515f5d47412a043ba768247

--- linker/linker_phdr.cpp
+++ linker/linker_phdr.cpp
@@ -118,7 +118,7 @@
 ElfReader::ElfReader(const char* name, int fd)
     : name_(name), fd_(fd),
       phdr_num_(0), phdr_mmap_(NULL), phdr_table_(NULL), phdr_size_(0),
-      load_start_(NULL), load_size_(0), load_bias_(0),
+      load_start_(NULL), load_size_(0), load_bias_(0), required_base_(0),
       loaded_phdr_(NULL) {
 }
 
@@ -277,6 +277,36 @@ size_t phdr_table_get_load_size(const Elf32_Phdr* phdr_table,
     return max_vaddr - min_vaddr;
 }
 
+typedef struct {
+    long mmap_addr;
+    char tag[4]; /* 'P', 'R', 'E', ' ' */
+} prelink_info_t;
+
+/* Returns the requested base address if the library is prelinked,
+ * and 0 otherwise.  */
+static Elf32_Addr is_prelinked(int fd, const char *name)
+{
+    off_t sz = lseek(fd, -sizeof(prelink_info_t), SEEK_END);
+    if (sz < 0) {
+        DL_ERR("lseek() failed!");
+        return 0;
+    }
+
+    prelink_info_t info;
+    int rc = TEMP_FAILURE_RETRY(read(fd, &info, sizeof(info)));
+    if (rc != sizeof(info)) {
+        DL_ERR("Could not read prelink_info_t structure for `%s`\n", name);
+        return 0;
+    }
+
+    if (memcmp(info.tag, "PRE ", 4)) {
+        DL_ERR("`%s` is not a prelinked library\n", name);
+        return 0;
+    }
+
+    return (unsigned long)info.mmap_addr;
+}
+
 // Reserve a virtual address range big enough to hold all loadable
 // segments of a program header table. This is done by creating a
 // private anonymous mmap() with PROT_NONE.
@@ -288,8 +318,14 @@ bool ElfReader::ReserveAddressSpace() {
     return false;
   }
 
+  required_base_ = is_prelinked(fd_, name_);
+
   uint8_t* addr = reinterpret_cast<uint8_t*>(min_vaddr);
   int mmap_flags = MAP_PRIVATE | MAP_ANONYMOUS;
+  if (required_base_ != 0) {
+    mmap_flags |= MAP_FIXED;
+    addr = (uint8_t*) required_base_;
+  }
   void* start = mmap(addr, load_size_, PROT_NONE, mmap_flags, -1, 0);
   if (start == MAP_FAILED) {
     DL_ERR("couldn't reserve %d bytes of address space for \"%s\"", load_size_, name_);
@@ -297,7 +333,7 @@ bool ElfReader::ReserveAddressSpace() {
   }
 
   load_start_ = start;
-  load_bias_ = reinterpret_cast<uint8_t*>(start) - addr;
+  load_bias_ = reinterpret_cast<uint8_t*>(start) - reinterpret_cast<uint8_t*>(min_vaddr);
   return true;
 }
 
