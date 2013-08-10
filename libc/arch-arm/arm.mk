6d128648dc36a5bed361d661dc35189e9c657118
Steve Kondik <shade@chemlab.org>
2013-08-09 21:56:10 -0700

    libc: Add Scorpion-optimized memcpy and memset
     * Optimized routines from Qualcomm for msm8660 class SoCs
     * Enable with TARGET_CPU_VARIANT := scorpion
    
    Change-Id: I01d0f22efba5a418ddd20fca0d0c570d855e0f6f
    
    libc: Use the fast strlen on Scorpion too
    
    Change-Id: I9ba50a469de27269adc008c2a96767830ad6b70b

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -38,7 +38,7 @@ libc_common_src_files := \
     $(filter-out $(_LIBC_FORTIFY_FILES_TO_REMOVE),$(libc_common_src_files))
 
 ifeq ($(strip $(wildcard bionic/libc/arch-arm/$(TARGET_CPU_VARIANT)/$(TARGET_CPU_VARIANT).mk)),)
-$(error "TARGET_CPU_VARIANT not set or set to an unknown value. Possible values are cortex-a7, cortex-a8, cortex-a9, cortex-a15, krait. Use generic for devices that do not have a CPU similar to any of the supported cpu variants.")
+$(error "TARGET_CPU_VARIANT not set or set to an unknown value. Possible values are cortex-a7, cortex-a8, cortex-a9, cortex-a15, krait, scorpion. Use generic for devices that do not have a CPU similar to any of the supported cpu variants.")
 endif
 
 include bionic/libc/arch-arm/$(TARGET_CPU_VARIANT)/$(TARGET_CPU_VARIANT).mk
