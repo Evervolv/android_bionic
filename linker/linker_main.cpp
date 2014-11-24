7ef0d6c7b3d56d0e86a802aec4833539fb2e23ed
Michael Bestas <mikeioannina@gmail.com>
2014-11-24 22:12:22 +0200

    Revert "Revert "Reenable support for non-PIE executables""
    * Conditionally revert under TARGET_NEEDS_NON_PIE_SUPPORT flag
    
    This reverts commit 76e289c026f11126fc88841b3019fd5bb419bb67.
    
    [mikeioannina]: Fix 5.1 compatibility
    6.0 compatibility fixes by Stefan Berger <s.berger81@gmail.com>
    8.0 compatibility fixes by Nick Reuter <nreuter85@gmail.com>
    
    Change-Id: I438b1f9f3dff2503a2dd51acbf2351d57f9e0f0b

--- linker/linker_main.cpp
+++ linker/linker_main.cpp
@@ -310,6 +310,7 @@ static ElfW(Addr) __linker_init_post_relocation(KernelArgumentBlock& args, ElfW(
   }
   si->dynamic = nullptr;
 
+#ifndef ENABLE_NON_PIE_SUPPORT
   ElfW(Ehdr)* elf_hdr = reinterpret_cast<ElfW(Ehdr)*>(si->base);
 
   // We haven't supported non-PIE since Lollipop for security reasons.
@@ -327,6 +328,7 @@ static ElfW(Addr) __linker_init_post_relocation(KernelArgumentBlock& args, ElfW(
                      g_argv[0]);
     exit(EXIT_FAILURE);
   }
+#endif
 
   // Use LD_LIBRARY_PATH and LD_PRELOAD (but only if we aren't setuid/setgid).
   parse_LD_LIBRARY_PATH(ldpath_env);
