6152ab5b2fc905245185173ae9eb7b503fd814f3
Nick Reuter <nreuter85@gmail.com>
2019-04-17 14:33:02 -0500

    libc: remove invalid pthread_t logspam
    Change-Id: I880b2efd185cf334e9825ce3e976b5fd39894af3

--- libc/bionic/pthread_internal.cpp
+++ libc/bionic/pthread_internal.cpp
@@ -115,14 +115,7 @@ pthread_internal_t* __pthread_internal_find(pthread_t thread_id) {
 
   // Historically we'd return null, but
   if (bionic_get_application_target_sdk_version() >= __ANDROID_API_O__) {
-    if (thread == nullptr) {
-      // This seems to be a common mistake, and it's relatively harmless because
-      // there will never be a valid thread at address 0, whereas other invalid
-      // addresses might sometimes contain threads or things that look enough like
-      // threads for us to do some real damage by continuing.
-      // TODO: try getting rid of this when Treble lets us keep vendor blobs on an old API level.
-      async_safe_format_log(ANDROID_LOG_WARN, "libc", "invalid pthread_t (0) passed to libc");
-    } else {
+    if (thread != nullptr) {
       async_safe_fatal("invalid pthread_t %p passed to libc", thread);
     }
   }
